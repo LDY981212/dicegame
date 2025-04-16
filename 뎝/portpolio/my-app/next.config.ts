b40987729b6d703c2813e70ee1557f6ee96a9746
LDY981212 <d653@naver.com>
2025-04-16 20:57:44 +0900

    init

--- 뎝/portpolio/my-app/next.config.ts
+++ 뎝/portpolio/my-app/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/LDY981212.github.io",
+  assetPrefix: "",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
